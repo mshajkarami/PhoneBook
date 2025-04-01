56d944bfc99c12cd8d81d1faa348cb16586eeb88
mshajkarami <mshajkarami.dev@gmail.com>
2025-04-01 19:09:00 +0330

    finish App

--- app/src/main/java/ir/hajkarami/phonebook/MyAdapter.java
+++ app/src/main/java/ir/hajkarami/phonebook/MyAdapter.java
@@ -1,11 +1,13 @@
 package ir.hajkarami.phonebook;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -13,25 +15,32 @@ import java.util.ArrayList;
 import ir.hajkarami.phonebook.databinding.ItemCardBinding;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.UserViewHolder> {
-    private Context mContext ;
+    private Context mContext;
     private ArrayList<User> mUserArrayList;
 
     public MyAdapter(Context context, ArrayList<User> userArrayList) {
         mContext = context;
         mUserArrayList = userArrayList;
     }
+
     @NonNull
     @Override
     public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        ItemCardBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_card, parent, false);
+
+
+        return new UserViewHolder(binding);
     }
+
     @Override
     public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
-
+        User currentUser = mUserArrayList.get(position);
+        holder.mItemCardBinding.setUser(currentUser);
     }
+
     @Override
     public int getItemCount() {
-        return 0;
+        return mUserArrayList.size();
     }
 
 
